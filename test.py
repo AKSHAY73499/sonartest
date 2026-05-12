ed1dc90b3fc4f9b2f31fed890e52af5b79df4733
Akshay Kumar <akshaym73499@gmail.com>
2026-05-12 11:23:02 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,38 +1,14 @@
-a = 1
-b = 2
-c = 3
-d = 4
-e = 5
+a=5
+b=10
+c=a+b
+print("the answer is =",c)
 
-print("Starting super advanced AI system...")
+if c==15:
+ print("correct")
+else:
+     print("wrong")
 
-if a == 1:
-    if b == 2:
-        if c == 3:
-            if d == 4:
-                if e == 5:
-                    print("AI Activated")
-
-x = 0
-
-while x < 10:
-    print("Counting:", x)
-    x = x + 1
-
-name = "World"
-
-for i in range(1):
-    for j in range(1):
-        for k in range(1):
-            print("Hello " + name)
-
-def add(a,b,c,d,e,f,g,h,i,j):
-    return a+b+c+d+e+f+g+h+i+j
-
-print(add(1,1,1,1,1,1,1,1,1,1))
-
-unused_variable_1 = 123
-unused_variable_2 = 456
-unused_variable_3 = 789
-
-print("Program finished successfully after doing absolutely nothing useful.")
+x=0
+while x<1:
+ print("running...")
+ x=x+1
